89850349f76945fb9c90235a94b8bf25dd290dbd
gneils <gregneils@gmail.com>
2017-11-22 11:17:12 -0800

    delete this commit
    Delete this commit

--- Instance_marker.php
+++ Instance_marker.php
@@ -22,7 +22,6 @@ use ExternalModules\ExternalModules;
  * The tab title is modified by prepending the first letter of the instance type.
  * 
  * Note: The live/production instance of REDCap should remain unmarked.
- * 
  */
 class Instance_marker extends \ExternalModules\AbstractExternalModule {
 
@@ -63,11 +62,6 @@ class Instance_marker extends \ExternalModules\AbstractExternalModule {
         return htmlspecialchars(strip_tags($other_text));
     }
 
-    /*
-     * REDCap logos vary in size depending on the page
-     * Sets the logo size and coordinates of the text
-     */
-    
     private function set_logo_size() {
         $this->logo_size = 'medium';
         
@@ -93,19 +87,13 @@ class Instance_marker extends \ExternalModules\AbstractExternalModule {
         }
     }
 
-/*
- * The instance is based on mathcing a portion of the Base URL.
- * Sets the instance type, display text and other text as applicable
- * Sets selected if the URL is matched.
- */
-
     private function select_instance() {
         $this->selected = 0;
         foreach ($this->instance_url as $key => $value) {
             if (strpos(APP_PATH_WEBROOT_FULL, $value) !== false) {
 
                 $this->instance = $key;
-                $this->display_text = ucfirst($key);
+                $this->display_text = $key;
                 $this->selected = 1;
                 if ($key === 'other') {
                     $this->display_text = $this->other_text;
@@ -116,9 +104,6 @@ class Instance_marker extends \ExternalModules\AbstractExternalModule {
        
     }
 
-    /* 
-     * Add the javascript to the top of every REDCap page
-     */
     public function redcap_every_page_top($project_id) {
         $this->init();
         if ($this->selected === 1) {
@@ -126,10 +111,6 @@ class Instance_marker extends \ExternalModules\AbstractExternalModule {
         }
     }
 
-    /*
-     * Instances are also indicated by color.
-     * Sets specific color based on instance
-     */
     private function instance_color() {
         $this->instance_color = '#000000';
         switch ($this->instance) {
@@ -154,9 +135,6 @@ class Instance_marker extends \ExternalModules\AbstractExternalModule {
         }
     }
 
-    /*
-     * Constructs the javascript that will run at the top of every page
-     */
     private function construct_js() {
         $display = '<div class=\"instance-type\" style=\"position:fixed;' .
                 'top:' . $this->offset_top . 'px; ' .
@@ -170,7 +148,7 @@ class Instance_marker extends \ExternalModules\AbstractExternalModule {
         $this->js = '<script>' .
                 '$(document).ready(function(){' . PHP_EOL . 
                 '  old_title = $("title").text(); ' .  PHP_EOL . 
-                '  new_title = "' . $this->display_text . '".substring(0, 1) + "-" + old_title; ' . PHP_EOL . 
+                '  new_title = old_title.replace("REDCap ","REDCap-' . $this->display_text . '")  ; ' . PHP_EOL . 
                 '  display_text = "' . $display . '";' .  PHP_EOL . 
                 '  $(document).prop("title", new_title);' .  PHP_EOL . 
                 '  $("body")' . PHP_EOL . 
