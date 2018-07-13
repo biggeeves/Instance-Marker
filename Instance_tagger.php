c6efe36eb656d5534fc9d0cec9cf810181e9af5c
DESKTOP-HE806NQ\Greg Neils <gregneils@gmail.com>
2018-07-13 16:55:47 -0700

    Added Option to set width in percent. Added code to make marker appear on every page. Reformated and cleaned code.

--- Instance_tagger.php
+++ Instance_tagger.php
@@ -13,17 +13,17 @@ use ExternalModules\AbstractExternalModule;
 use ExternalModules\ExternalModules;
 
 /**
- * A single site may have multiple instances of REDCap in use. 
+ * A single site may have multiple instances of REDCap.
  * Examples: Production, Backup, Development instances.
  * Instances differentiated by a unique URL
- * will have the instance marked below the REDCap logo
- * indicating the specific instance based on the URL.
+ * will have the instance clearly marked on every page based on the URL.
  * The tab title is modified by prepending the first letter of the instance type.
- * 
+ *
  * Note: The live/production instance of REDCap should remain unmarked.
- * 
+ *
  */
-class Instance_tagger extends \ExternalModules\AbstractExternalModule {
+class Instance_tagger extends \ExternalModules\AbstractExternalModule
+{
 
     private $offset_top;
     private $offset_right;
@@ -37,13 +37,24 @@ class Instance_tagger extends \ExternalModules\AbstractExternalModule {
     private $other_text;
     private $debug_js;
     private $debug_mode = false;
+    private $height;
+    private $width;
+    private $width_pixels;
+    private $width_percent;
+    private $display_text;
+    private $width_percent_helper;
+    private $js;
 
-    public function init() {
+    /**
+     *
+     */
+    public function init()
+    {
 
         $this->get_settings();
         $this->set_other_text();
         $this->set_location();
-        $this->set_height_width();
+        $this->set_height_and_width();
         $this->select_instance();
         $this->set_bk_color();
         $this->set_opacity();
@@ -51,8 +62,12 @@ class Instance_tagger extends \ExternalModules\AbstractExternalModule {
         $this->construct_js();
     }
 
-    // Get stored settings
-    private function get_settings() {
+
+    /**
+     * Fetch user stored settings from REDCap
+     */
+    private function get_settings()
+    {
         $this->instance_url['localhost'] = AbstractExternalModule::getSystemSetting('localhost_url');
         $this->instance_url['development'] = AbstractExternalModule::getSystemSetting('development_url');
         $this->instance_url['testing'] = AbstractExternalModule::getSystemSetting('testing_url');
@@ -65,15 +80,19 @@ class Instance_tagger extends \ExternalModules\AbstractExternalModule {
         $this->offset_right = AbstractExternalModule::getSystemSetting('offset_right');
         $this->offset_bottom = AbstractExternalModule::getSystemSetting('offset_bottom');
         $this->offset_left = AbstractExternalModule::getSystemSetting('offset_left');
-        $this->height = (int) AbstractExternalModule::getSystemSetting('height');
-        $this->width = (int) AbstractExternalModule::getSystemSetting('width');
+        $this->height = AbstractExternalModule::getSystemSetting('height');
+        $this->width_pixels = AbstractExternalModule::getSystemSetting('width');
+        $this->width_percent = AbstractExternalModule::getSystemSetting('width_percent');
         $this->bk_color = AbstractExternalModule::getSystemSetting('bk_color');
         $this->bk_color = htmlspecialchars(strip_tags($this->bk_color));
-        $this->opacity = (int) AbstractExternalModule::getSystemSetting('opacity');
+        $this->opacity = AbstractExternalModule::getSystemSetting('opacity');
     }
 
-    // Set other text to default "Other" if blank.
-    private function set_other_text() {
+    /**
+     * Set other text to default "Other" if blank.
+     */
+    private function set_other_text()
+    {
         if (($this->other_text === '') || (is_null($this->other_text))) {
             $this->other_text = 'Other';
         }
@@ -83,65 +102,86 @@ class Instance_tagger extends \ExternalModules\AbstractExternalModule {
      * Set location of top, right, left, and bottom fixed points
      */
 
-    private function set_location() {
-        if (!is_null($this_top)) {
-            if ($this->offset_top <= 0 || $this->offset_top > 2000) {
-                $this->offset_top = 45;
+    /**
+     *
+     */
+    private function set_location()
+    {
+        if (!is_null($this->offset_top)) {
+            if ($this->offset_top < 0 || $this->offset_top > 2000) {
+                $this->offset_top = null;
             }
         }
-        if (!is_null($this_right)) {
-            if ($this->offset_right <= 0 || $this->offset_right > 2000) {
-                $this->offset_right = 18;
+        if (!is_null($this->offset_right)) {
+            if ($this->offset_right < 0 || $this->offset_right > 2000) {
+                $this->offset_right = null;
             }
         }
-        if (!is_null($this_bottom)) {
-            if ($this->offset_bottom <= 0 || $this->offset_bottom > 2000) {
-                $this->offset_bottom = 45;
+        if (!is_null($this->offset_bottom)) {
+            if ($this->offset_bottom < 0 || $this->offset_bottom > 2000) {
+                $this->offset_bottom = null;
             }
         }
-        if (!is_null($this_left)) {
-            if ($this->offset_left <= 0 || $this->offset_left > 2000) {
-                $this->offset_left = 18;
+        if (!is_null($this->offset_left)) {
+            if ($this->offset_left < 0 || $this->offset_left > 2000) {
+                $this->offset_left = null;
             }
+        }
+        if(is_null($this->offset_top) && is_null($this->offset_bottom)) {
+            $this->offset_bottom = 0;
         }
     }
 
-    /*
-     * Set the opacity of the marker. 
+    /**
+     * Set the opacity of the marker.
      */
-
-    private function set_opacity() {
-        if ($this->opacity <= 0 || $this->offset_top >= 100) {
+    private function set_opacity()
+    {
+        if ($this->opacity <= 0 || $this->offset_top > 100) {
             $this->opacity = 1;
         } else {
             $this->opacity = $this->opacity / 100;
         }
     }
 
-    /*
-     * Set Height and width of marker
+    /**
+     * Set Height and Width of marker
+     * The default for width is in percent.
      */
-
-    private function set_height_width() {
-        if ($this->width <= 15 || $this->width > 1000) {
-            $this->width = "";
+    private function set_height_and_width()
+    {
+        // Set Width
+        $this->width_percent_helper = "";
+        if (is_null($this->width_percent) && is_null($this->width_pixels)) {
+            $this->width = "100%";
         } else {
-            $this->width .= "px";
+            if ($this->width_percent >= 0 &&
+                $this->width_percent <= 100 &&
+                !is_null($this->width_percent)) {
+                $this->width = $this->width_percent . '%';
+                $this->width_percent_helper = 'margin-left: ' . (100 - $this->width_percent) / 2 . '%;';
+            } else if ($this->width_pixels >= 5 || $this->width_pixels <= 1000) {
+                $this->width = $this->width_pixels . "px";
+            } else {
+                $this->width = "100%";
+            }
         }
-        if ($this->height <= 15 || $this->height > 800) {
-            $this->height = "";
+
+        // Set Height
+        if ($this->height < 5 || $this->height >= 800) {
+            $this->height = "25px";
         } else {
             $this->height .= "px";
         }
     }
 
-    /*
+
+    /**
      * Compare Base URL to Instance URLs in settings.
      * Select the marker and text to be displayed
-     * 
      */
-
-    private function select_instance() {
+    private function select_instance()
+    {
         $this->selected = 0;
         foreach ($this->instance_url as $key => $value) {
             if (strpos(APP_PATH_WEBROOT_FULL, $value) !== false) {
@@ -157,22 +197,28 @@ class Instance_tagger extends \ExternalModules\AbstractExternalModule {
         }
     }
 
-    public function redcap_every_page_top($project_id) {
+    /**
+     * Add css and js to every page
+     * if in debug mode add debug script
+     *
+     * @param $project_id
+     */
+    public function redcap_every_page_top($project_id)
+    {
         $this->init();
         if ($this->selected === 1) {
             echo $this->js;
-            echo $this->css;
         }
         if ($this->get_debug_mode() == 1) {
             echo $this->debug_js;
         }
     }
 
-    /*
-     * Set background color based on instance if it was not specified
+    /**
+     * Set background color based on instance
      */
-
-    private function set_bk_color() {
+    private function set_bk_color()
+    {
 
         if (is_null($this->bk_color) || $this->bk_color == "") {
             $this->bk_color = '#000000';
@@ -187,10 +233,10 @@ class Instance_tagger extends \ExternalModules\AbstractExternalModule {
                     $this->bk_color = '#835C3B';  // Brown
                     break;
                 case 'backup':
-                    $this->bk_color = '#DD3236';  // Redish
+                    $this->bk_color = '#DD3236';  // Reddish
                     break;
                 case 'staging':
-                    $this->bk_color = '#BA9215'; //  Goldish
+                    $this->bk_color = '#BA9215'; //  Gold
                     break;
                 case 'other':
                     $this->bk_color = '#f0ad4e'; // Orange
@@ -200,80 +246,102 @@ class Instance_tagger extends \ExternalModules\AbstractExternalModule {
 
     }
 
-    public function set_debug_mode($mode) {
+    /**
+     * Set Debug Mode 0=Off, 1=On
+     * @param $mode
+     */
+    public function set_debug_mode($mode)
+    {
         $this->debug_mode = $mode;
     }
 
-    public function get_debug_mode() {
+    /**
+     * Return Debug Mode.  0=Off, 1=On
+     * @return bool
+     */
+    public function get_debug_mode()
+    {
         return $this->debug_mode;
     }
 
-    /*
-     * Output value to console for easier debugging.
-     */
 
-    private function construct_js() {
+    /**
+     * Settings instance-type to settings from REDCap.
+     * Output value and settings to console for easier debugging.
+     */
+    private function construct_js()
+    {
         $css_styles = '.instance-type {' .
-                'position:fixed;' .
-                'top: ' . $this->offset_top . 'px; ' .
-                'right: ' . $this->offset_right . 'px;' .
-                'bottom: ' . $this->offset_bottom . 'px;' .
-                'left: ' . $this->offset_left . 'px;' .
-                'color: ' . $this->color . ';' .
-                'opacity: ' . $this->opacity . ';' .
-                'height: ' . $this->height . ';' .
-                'width: ' . $this->width . ';' .
-                'background-color:' . $this->bk_color . ';' . 'font-weight:bold;' .
-                'font-style:italic;' .
-                'z-index:1040;' .
-                'color: white;' .
-                'border-radius: 2px;' .
-                'padding: 5px;' .
-                'text-align: center;' .
-                '}';
+            'position:fixed;';
+        if (!is_null($this->offset_top)) {
+            $css_styles .= 'top: ' . $this->offset_top . 'px; ';
+        }
+        if (!is_null($this->offset_right)) {
+            $css_styles .= 'right: ' . $this->offset_right . 'px;';
+        }
+        if (!is_null($this->offset_bottom)) {
+            $css_styles .= 'bottom: ' . $this->offset_bottom . 'px;';
+        }
+        if (!is_null($this->offset_left)) {
+            $css_styles .= 'left: ' . $this->offset_left . 'px;';
+        }
+        if (!is_null($this->opacity)) {
+            $css_styles .= 'opacity: ' . $this->opacity . ';';
+        }
+        $css_styles .= 'height: ' . $this->height . ';' .
+            'width: ' . $this->width . ';' .
+            $this->width_percent_helper .
+            'background-color:' . $this->bk_color . ';' .
+            'font-weight:bold;' .
+            'font-style:italic;' .
+            'z-index:1040;' .
+            'color: white;' .
+            'border-radius: 2px;' .
+            'padding: 5px;' .
+            'text-align: center;' .
+            '}';
 
 
         $style_sheet = '<style type="text/css">' . $css_styles . '</style>';
 
         $display = '<div class=\"instance-type\">' .
-                $this->display_text .
-                '</div>';
+            $this->display_text .
+            '</div>';
 
         $this->js = '<script>' .
-                '$(document).ready(function(){' . PHP_EOL .
-                '  $(\'' . $style_sheet . '\').appendTo("head");' . PHP_EOL .
-                '  old_title = $("title").text(); ' . PHP_EOL .
-                '  new_title = "' . $this->display_text . '".substring(0, 1) + "-" + old_title; ' . PHP_EOL .
-                '  text_div = "' . $display . '";' . PHP_EOL .
-                '  $(document).prop("title", new_title);' . PHP_EOL .
-                '  $("body")' . PHP_EOL .
-                '.prepend(text_div);' . PHP_EOL .
-                '});' . PHP_EOL .
-                '</script>';
+            '$(document).ready(function(){' .
+            '$(\'' . $style_sheet . '\').appendTo("head");' .
+            'old_title = $("title").text(); ' .
+            'new_title = "' . $this->display_text . '".substring(0, 1) + "-" + old_title; ' .
+            'text_div = "' . $display . '";' .
+            '$(document).prop("title", new_title);' .
+            '$("body").prepend(text_div);' .
+            '$(".instance-type").click(function(){$(".instance-type").hide();})'.
+            '});' . PHP_EOL .
+            '</script>';
     }
 
-    /*
-     * Construct debug information displayed in console window.
-     */
 
-    private function set_debug_js() {
+    /**
+     * debug information displayed in console window.
+     */
+    private function set_debug_js()
+    {
         $this->debug_js = '<script>' .
-                '$(document).ready(function(){' . PHP_EOL .
-                'console.log("Instance Tagger ran"); ' .
-                'console.log("' . 'top:' . $this->offset_top . 'px; ' . '"); ' .
-                'console.log("' . 'right:' . $this->offset_right . 'px;' . '"); ' .
-                'console.log("' . 'bottom:' . $this->offset_bottom . 'px; ' . '"); ' .
-                'console.log("' . 'left:' . $this->offset_left . 'px;' . '"); ' .
-                'console.log("' . 'color:' . $this->bk_color . '"); ' .
-                'console.log("' . 'opacity:' . $this->opacity . '"); ' .
-                'console.log("' . 'height:' . $this->height . '"); ' .
-                'console.log("' . 'width:' . $this->width . '"); ' .
-                'console.log("' . 'text:' . $this->display_text . '"); ' .
-                'h = $("img[alt=\'REDCap\']").height();' .
-                'w = $("img[alt=\'REDCap\']").width();' .
-                'console.log("Height: " + h + " Width: " + w); ' .
-                '});' . PHP_EOL .
-                '</script>';
+            '$(document).ready(function(){' . PHP_EOL .
+            'console.log("Instance Tagger ran"); ' .
+            'console.log("' . 'top:' . $this->offset_top . 'px; ' . '"); ' .
+            'console.log("' . 'right:' . $this->offset_right . 'px;' . '"); ' .
+            'console.log("' . 'bottom:' . $this->offset_bottom . 'px; ' . '"); ' .
+            'console.log("' . 'left:' . $this->offset_left . 'px;' . '"); ' .
+            'console.log("' . 'bg_color:' . $this->bk_color . '"); ' .
+            'console.log("' . 'opacity:' . $this->opacity . '"); ' .
+            'console.log("' . 'height:' . $this->height . '"); ' .
+            'console.log("' . 'width:' . $this->width . '"); ' .
+            'console.log("' . 'width percent helper:' . $this->width_percent_helper . '"); ' .
+            'console.log("' . 'text:' . $this->display_text . '"); ' .
+            '});' . PHP_EOL .
+            '</script>';
     }
 
 }
--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-<h2>Instance Marker - Display information about which instance of REDCap is being used.</h2>
+<h2>Instance Marker - Clearly differentiate instances of REDCap</h2>
 
-<p>A single site may have multiple instances of REDCap that need to be clearly marked. Examples include Production, Backup and Development. By pattern matching the URL to the instance a small bit of matching text will be displayed in the top right corner of every page. Also, the tab title is prefixed with the first letter of the instance type. The module must be enabled on all projects by default in order for the instance label to appear inside projects or must be enabled on a project by project basis.</p>
+<p>A single site may have multiple instances of REDCap that need to be easily differentiated. 
+Various instances may include Production, Backup and Development. 
+When a pattern in the URL matches a specific instance unobtrusive text specifying the instance is displayed on every page. 
+The tab title is prefixed with the first letter of the instance type.  Thus the production instance and the development
+instance can be easily and effiecently differentiated. 
 
-<p>The goal is to easily identify which instance of REDCap you are currently in.</p>
+The module must be enabled on all projects by default in order for the instance label to appear inside 
+projects or must be enabled on a project by project basis.</p>
 
-<p>The marker can be placed anywhere on the screen.  The marker is fixed in that position. The image below is an example of how the marker will look when fixed to the bottom, left and right of the window. The opacity can be set.  The image below has opacity set to 80 (on a scale of 0 to 100).</p>
+<p>The marker can be placed anywhere on the screen. The example below is how the marker will look when fixed to the bottom of the window using the follwoing settings</p>
+<ul><li>Bottom=0</li></li>Width=100%</li><li>Opacity=80</li></ul>
 <img src="https://github.com/biggeeves/Instance-Marker/blob/master/images/instance_tagger_screen_shot.gif">
